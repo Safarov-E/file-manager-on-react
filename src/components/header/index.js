fd57a54b58f9efbac1410300c832fd2690ea7aa4
Elkhan <elkhan.safarov10@yandex.ru>
2019-10-02 13:57:06 +0500

    Страница 404

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -11,8 +11,9 @@ export default class Header extends React.Component {
                     <a className='navbar-brand' href='#'>Brand</a> 
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className='navbar-nav mr-auto'>
-                            <li className='nav-item active'><Link to='/'>Home</Link></li>
-                            <li className='nav-item dropdown'><Link to='/contact'>Contact</Link></li>
+                            <li className='nav-item active' style={{paddingRight: "12px"}}><Link to='/'>Home</Link></li>
+                            <li className='nav-item dropdown' style={{paddingRight: "12px"}}><Link to='/contact'>Contact</Link></li>
+                            <li className='nav-item dropdown' style={{paddingRight: "12px"}}><Link to='/contacts'>Error page</Link></li>
                         </ul>
                     </div> 
             </nav> 
